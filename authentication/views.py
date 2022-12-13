9420e507dc3c6a4313dbde130589ec252ec37b16
sivaprasad <sivaprasad121333@gmail.com>
2022-12-13 20:15:25 +0530

    more changes

---
+++ authentication/views.py
@@ -0,0 +1,132 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail import EmailMessage, send_mail
+from user_login import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth import authenticate, login, logout
+from . tokens import generate_token
+
+# Create your views here.
+def home(request):
+    return render(request, "authentication/index.html")
+
+def signup(request):
+
+    if request.method == "POST":
+        #username=request.POST.get('username')
+        username=request.POST['username']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        email=request.POST['email']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username")
+            return redirect('home') 
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('home')
+
+        if len(username)>10:
+            message.error(request, "Username must be under 10 characters")
+
+        if pass1 !=pass1:
+            message.error(request, "Passwords didn't match!")
+
+        if not username.isalnum():
+            message.error(request, "Username must be Alpha-Numeric!")
+            return redirect('home')
+
+   
+
+
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name=fname 
+        myuser.last_name=lname
+        myuser.is_active=False
+        myuser.save()
+
+        messages.success(request,"Your Account has been successfully created. We have sent you a confirmation email,please confirm your email in order to activate your account. ") 
+
+        # Welcome Email
+
+        subject = "Welcome to  User- Django Login!! "
+        message = "Hello "+ myuser.first_name + "!! \n" +  "Welcome to Login!! \n Thank you for visiting our website \n we have also sent you a confirmation email,please confirm your email address in order to activate your account. \n\n Thanking you\n SIVA PRASAD"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True )
+
+
+        # Email Address Confirmation Email
+        current_site = get_current_site(request)
+        emai_subject = "confirm your email @ User- Django Login!!"
+        message2 = render_to_string('email_confirmation.html',{
+            "name": myuser.first_name,
+            "domain": current_site.domain,
+            "uid": urlsafe_base64_encode(force_bytes(myuser.pk)),
+            "token": generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True 
+        email.send()
+      
+
+        return redirect('signin')
+
+    return render(request, "authentication/signup.html")
+
+
+def signin(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        pass1=request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1) 
+
+        if user is not None:
+            login(request,user)
+            fname=user.first_name 
+            return render(request,"authentication/index.html", {'fname':fname}) 
+
+        else:
+            messages.error(request, "Bad/Wrong Credentials!" ) 
+            return redirect('home')
+
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request,"Logged Out Successfully!")
+    return redirect('home') 
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    
+    else:
+        return render(request, "activation_failed.html")
+
+
---
+++ user_login/info.py
@@ -0,0 +1,7 @@
+EMAIL_USE_TLS=True 
+EMAIL_HOST='smtp.gmail.com' # Simple Mail Transfer Protocol :
+                            # An SMTP server is a computer or an app that is responsible for sending emails
+EMAIL_HOST_USER='siva.userlogin.django@gmail.com'
+EMAIL_HOST_PASSWORD='121333'
+EMAIL_PORT=587 
+
